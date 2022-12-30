7d364f4b544c9385bf0dc316ceb3ea6a2ac4f8e2
kgreyy <57283823+kgreyy@users.noreply.github.com>
2022-12-31 03:46:34 +0800

    Revise db table schema

--- server/database/migrations/sqls/20221119181152-init-tables-up.sql
+++ server/database/migrations/sqls/20221119181152-init-tables-up.sql
@@ -1,54 +1,93 @@
 /* Replace with your SQL commands */
-CREATE TABLE IF NOT EXISTS `user`(
-    `user_id` VARCHAR(14) NOT NULL,
-    `user_name` VARCHAR(15) NOT NULL,
-    `user_email` VARCHAR(320) NOT NULL,
-    `user_password` VARCHAR (128) NOT NULL,
+CREATE TABLE IF NOT EXISTS `users`(
+    `user_id` INT NOT NULL,
+    `user_name` VARCHAR(100) NOT NULL,
+    `email` VARCHAR(100) NOT NULL,
+    `password` VARCHAR (128) NOT NULL,
     `user_dp` VARCHAR(255) NOT NULL,
-    `first_name` VARCHAR(64),
-    `last_name` VARCHAR(64),
-    `birthday` BIGINT NOT NULL,
-    `age` INT NOT NUll,
-    `teacher` TINYINT(1),
+    `first_name` VARCHAR(50) NOT NULL,
+    `last_name` VARCHAR(50) NOT NULL,
+    `role` CHAR(1) NOT NULL,
     PRIMARY KEY (`user_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `course`(
-    `course_id` VARCHAR(14) NOT NULL,
-    `course_name` VARCHAR(15) NOT NULL,
-    `student_id` VARCHAR(14),
-    `teacher_id` VARCHAR (14) NOT NULL,
-    `teacher_name` VARCHAR (128) NOT NULL,
-    `course_desc` VARCHAR(255),
-    `enrollment_key` VARCHAR(128) NOT NULL,
-    PRIMARY KEY (`course_id`, `student_id`)
+    `course_id` INT NOT NULL,
+    `course_name` VARCHAR(100) NOT NULL,
+    `course_desc` VARCHAR(255) NOT NULL,
+    `enrollment_key` VARCHAR(30) NOT NULL,
+    `teacher_id` INT NOT NULL,
+    PRIMARY KEY (`course_id`),
+    FOREIGN KEY (`teacher_id`) REFERENCES users(`user_id`) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS `course_enroll`(
+    `course_id` INT NOT NULL,
+    `student_id` INT NOT NULL,
+    PRIMARY KEY (`course_id`, `student_id`),
+    FOREIGN KEY (`course_id`) REFERENCES course(`course_id`) ON DELETE CASCADE,
+    FOREIGN KEY (`student_id`) REFERENCES users(`user_id`) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS `pset`(
-    `pset_id` VARCHAR(14) NOT NULL,
-    `course_id` VARCHAR(14) NOT NULL,
-    `student_id` VARCHAR(14) NOT NULL,
-    `pset_num` INT NOT NULL,
-    `pset_title` VARCHAR(128) NOT NULL,
-    `pset_desc` VARCHAR(255),
-    `start_time` BIGINT,
-    `duration` BIGINT,
-    `due_date` BIGINT,
-    `submit_time` BIGINT,
-    `items_count` INT,
-    `score` FLOAT,
-    PRIMARY KEY (`pset_id`, `student_id`)
+    `pset_id` INT NOT NULL,
+    `course_id` INT NOT NULL,
+    `pset_title` VARCHAR(100) NOT NULL,
+    `pset_desc` VARCHAR(255) NOT NULL,
+    `pset_order` SMALLINT NOT NULL,
+    `due_datetime` DATETIME NOT NULL,
+    `published` CHAR(1) NOT NULL,
+    `float_tolerance` FLOAT NOT NULL,
+    PRIMARY KEY (`pset_id`, `course_id`),
+    FOREIGN KEY (`course_id`) REFERENCES course(`course_id`) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS `item`(
-    `item_id` VARCHAR(14) NOT NULL,
-    `pset_id` VARCHAR(14) NOT NULL,
-    `student_id` VARCHAR(14) NOT NULL,
-    `item_num` INT NOT NULL,
+    `item_id` INT NOT NULL,
+    `pset_id` INT NOT NULL,
+    `course_id` INT NOT NULL,
+    `item_order` SMALLINT NOT NULL,
     `problem` VARCHAR(1000) NOT NULL,
-    `correct_answer` VARCHAR(1000) NOT NULL,
-    `student_answer` VARCHAR(1000),
-    `feedback` VARCHAR(1000),
-    `score` FLOAT,
-    PRIMARY KEY (`item_id`, `student_id`)
+	`type` CHAR(1) NOT NULL,
+    `points` FLOAT NOT NULL,
+    `parent` INT,
+    `post_problem` VARCHAR(300),
+    PRIMARY KEY (`pset_id`, `course_id`),
+    FOREIGN KEY (`pset_id`) REFERENCES pset(`pset_id`) ON DELETE CASCADE,
+    FOREIGN KEY (`course_id`) REFERENCES course(`course_id`) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS `attempt`(
+    `item_id` INT NOT NULL,
+    `pset_id` INT NOT NULL,
+    `course_id` INT NOT NULL,
+    `student_id` INT NOT NULL,
+    `attempt_id` INT NOT NULL,
+    `student_feedback` VARCHAR(1000),
+	`is_correct` CHAR(1) NOT NULL,
+    `mulcho_answer` CHAR(1),
+    `submit_datetime` DATETIME NOT NULL,
+    PRIMARY KEY (`item_id`, `pset_id`, `course_id`, `student_id`, `attempt_id`),
+    PRIMARY KEY (`item_id`, `pset_id`, `course_id`, `student_id`),
+    FOREIGN KEY (`item_id`) REFERENCES item(`item_id`) ON DELETE CASCADE,
+    FOREIGN KEY (`pset_id`) REFERENCES pset(`pset_id`) ON DELETE CASCADE,
+    FOREIGN KEY (`course_id`) REFERENCES course(`course_id`) ON DELETE CASCADE,
+    FOREIGN KEY (`student_id`) REFERENCES users(`user_id`) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS `feedback`(
+    `item_id` INT NOT NULL,
+    `pset_id` INT NOT NULL,
+    `course_id` INT NOT NULL,
+    `feedback_id` INT NOT NULL,
+    `feedback` VARCHAR(1000) NOT NULL,
+	`is_correct` CHAR(1) NOT NULL,
+    `mulcho_answer` CHAR(1),
+    `float_answer` FLOAT,
+    `submit_datetime` DATETIME NOT NULL,
+    PRIMARY KEY (`item_id`, `pset_id`, `course_id`, `student_id`, `feedback_id`),
+    PRIMARY KEY (`item_id`, `pset_id`, `course_id`, `student_id`),
+    FOREIGN KEY (`item_id`) REFERENCES item(`item_id`) ON DELETE CASCADE,
+    FOREIGN KEY (`pset_id`) REFERENCES pset(`pset_id`) ON DELETE CASCADE,
+    FOREIGN KEY (`course_id`) REFERENCES course(`course_id`) ON DELETE CASCADE
 );
